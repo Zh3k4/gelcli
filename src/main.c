12ce9a9a927903c4825d317492dd633e5d79b3d8
Zh3k4 <henz20131@gmail.com>
2024-02-01 20:24:26 +0300

    implemennt barebones file download

--- src/main.c
+++ src/main.c
@@ -4,10 +4,18 @@
 #include <string.h>
 
 #include "jsmn.h"
+#include "slice.h"
 
 struct Mem {
 	char *memory;
 	size_t size;
+};
+
+struct RemoteFile {
+	const char *url;
+	int urlLen;
+	const char *filename;
+	int filenameLen;
 };
 
 const char *const gel = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&apikey=%s&tags=%s";
@@ -34,28 +42,21 @@ write_memory_func(void *content, size_t membsize, size_t nmemb, void *userp)
 	return size;
 }
 
-static int
-run(void)
+static struct Mem
+perform_api_call(void)
 {
-	int result;
+	struct Mem result = { 0 };
 	CURL *curl;
 	CURLcode curlcode;
 
-	curl_global_init(CURL_GLOBAL_ALL);
 	curl = curl_easy_init();
-	if (!curl) {
-		result = 0;
-		goto out;
-	}
+	if (!curl) goto out;
 
 	struct Mem mem;
 	mem.memory = malloc(1);
 	mem.size = 0;
 
-	if (!mem.memory) {
-		result = 0;
-		goto out_curl;
-	}
+	if (!mem.memory) goto out_curl;
 
 	snprintf(reqbuf, 2048, gel, "", "1girl");
 	curl_easy_setopt(curl, CURLOPT_URL, reqbuf);
@@ -67,9 +68,117 @@ run(void)
 	if (curlcode != CURLE_OK) {
 		fprintf(stderr, "Error: %s\n",
 			curl_easy_strerror(curlcode));
-		result = 0;
 		goto out_mem;
 	}
+
+	result = mem;
+	goto out_curl;
+
+out_mem:
+	free(mem.memory);
+out_curl:
+	curl_easy_cleanup(curl);
+out:
+
+	return result;
+}
+
+static int
+iseq_tok_cstr(const char *const mem, jsmntok_t tok, const char *const s)
+{
+	if (tok.type != JSMN_STRING) return 0;
+
+	int tlen = tok.end - tok.start;
+	int slen = strlen(s);
+	if (tlen != slen) return 0;
+
+	const char *const t = &mem[tok.start];
+	return strncmp(t, s, tlen) == 0;
+}
+
+struct RemoteFile
+get_pic(const char *const mem, int ntok, jsmntok_t tokens[ntok])
+{
+	for (int i = 1; i < ntok; i += 1) {
+		jsmntok_t t = tokens[i];
+		if (!iseq_tok_cstr(mem, t, "post")) continue;
+
+		jsmntok_t url = tokens[i + 2 + 44];
+		jsmntok_t fn = tokens[i + 2 + 16];
+
+		return (struct RemoteFile){
+			.url = &mem[url.start],
+			.urlLen = url.end - url.start,
+			.filename = &mem[fn.start],
+			.filenameLen = fn.end - fn.start,
+		};
+	}
+
+	return (struct RemoteFile){ 0 };
+}
+
+char *
+unescape_str(int len, const char str[len])
+{
+	char *const new = calloc(len + 1, 1);
+	if (!new) return NULL;
+
+	const char *s = (const char *)str;
+	const char *ss = s;
+	char *n = new;
+
+	while (s - ss < len) {
+		if (*s == '\\') s++;
+		*n++ = *s++;
+	}
+
+	return new;
+}
+
+static int
+download_file(const char *const url, const char *const filepath)
+{
+	int result = 0;
+
+	FILE *file = fopen(filepath, "wb");
+	if (!file) goto out;
+
+	CURL *curl;
+	CURLcode curlcode;
+
+	curl = curl_easy_init();
+	if (!curl) goto out_file;
+
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+
+	curlcode = curl_easy_perform(curl);
+
+	if (curlcode != CURLE_OK) {
+		fprintf(stderr, "Error: %s\n",
+			curl_easy_strerror(curlcode));
+		goto out_curl;
+	}
+
+	result = 1;
+
+out_curl:
+	curl_easy_cleanup(curl);
+out_file:
+	fclose(file);
+out:
+	return result;
+}
+
+static int
+run(void)
+{
+	int result = 0;
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	struct Mem mem = perform_api_call();
+	if (!mem.memory) goto out;
 
 	jsmn_parser p;
 	jsmn_init(&p);
@@ -93,16 +202,25 @@ run(void)
 		result = 0; goto out_tokens;
 	}
 
-	/* We've got the tokens! */
+	struct RemoteFile rf = get_pic(mem.memory, ntok, tokens);
 
-	result = 1;
+	char *url = unescape_str(rf.urlLen, rf.url);
+	char *filename = unescape_str(rf.filenameLen, rf.filename);
+	int success = download_file(url, filename);
+	free(filename);
+	free(url);
+
+	if (success) {
+		printf("Downloaded file: %.*s\n", rf.filenameLen, rf.filename);
+		result = 1;
+	} else {
+		printf("Couldn't download file\n");
+	}
 
 out_tokens:
 	free(tokens);
 out_mem:
 	free(mem.memory);
-out_curl:
-	curl_easy_cleanup(curl);
 out:
 	curl_global_cleanup();
 	return result;
