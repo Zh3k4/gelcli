c41efa8206b1855eac085d298efd58f01f4e36e6
Zh3k4 <henz20131@gmail.com>
2024-02-02 13:35:04 +0300

    fix typedef.h

--- src/typedef.h
+++ src/typedef.h
@@ -3,22 +3,22 @@
 
 #include "inttypes.h"
 
-typedef int8_t   s8
-typedef int16_t  s16
-typedef int32_t  s32
-typedef int64_t  s64
+typedef int8_t   s8;
+typedef int16_t  s16;
+typedef int32_t  s32;
+typedef int64_t  s64;
 
-typedef uint8_t  u8
-typedef uint16_t u16
-typedef uint32_t u32
-typedef uint64_t u64
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
-typedef intmax_t smax
-typedef uintmax_t umax
+typedef intmax_t smax;
+typedef uintmax_t umax;
 
-typedef size_t usize
+typedef size_t usize;
 
-typedef float f32
-typedef double f64
+typedef float f32;
+typedef double f64;
 
 #endif /* TYPEDEF_H_ */
