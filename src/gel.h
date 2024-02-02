c72df1c9a8aa82c25a7d88e26ca64785816bf121
Zh3k4 <henz20131@gmail.com>
2024-02-02 14:10:42 +0300

    gel: expose download_post

--- src/gel.h
+++ src/gel.h
@@ -6,6 +6,7 @@ struct GelPost;
 
 struct GelCtx gel_create(const char *const key, const char *const tags);
 void gel_destroy(struct GelCtx c);
+int download_post(struct GelPost p);
 struct GelPost get_post(struct GelCtx c);
 
 #endif /* GEL_H_ */
